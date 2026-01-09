9ce70b20ea906c0d7d81d529905f06bf5f5ca82e
kylectrent <kylectrent@gmail.com>
2026-01-08 22:27:55 -0800

    feat: initial commit, with docker-compose file

---
+++ src/main/java/com/kyletrent/veritone/VeritoneApplication.java
@@ -0,0 +1,13 @@
+package com.kyletrent.veritone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeritoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeritoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyletrent/veritone/VeritoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyletrent.veritone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeritoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
