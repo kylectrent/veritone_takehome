8af49af5661b2617c0a22049c0d968c04bad0c17
kylectrent <kylectrent@gmail.com>
2026-01-21 21:16:05 -0800

    feat: complete dockerization of front end, nginix proxy, end to end connectivity with get call. now to build UI :)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useShoppingList } from './features/shopping/api/hooks'
 
 function App() {
   const [count, setCount] = useState(0)
 
   // call get api/shopping get and display list below
+  const { items } = useShoppingList();
+
+  console.log(items);
 
   return (
     <>
